05b6b5c2b2722f63d59034d142cd5b71c73c2e2a
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-02-10 18:19:48 +0530

    Update createFile.py

--- createFile.py
+++ createFile.py
@@ -5,3 +5,12 @@ L = ["This is Delhi \n","This is Paris \n","This is London \n"]
 file1.write("Hello \n")
 file1.writelines(L)
 file1.close()
+
+file1 = open("other.txt","w")
+L = ["This is Pune \n","This is Paris \n","This is London \n"] 
+  
+# \n is placed to indicate EOL (End of Line)
+file1.write("Hello \n")
+file1.writelines(L)
+file1.close()
+
